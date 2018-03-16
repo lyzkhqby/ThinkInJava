045a8ce59d4793b2698c62e4875e8f65fdfe66f9
rotoosoft-zk <lyzkhqby@sina.com>
2018-03-16 18:07:24 +0800

    strings

---
+++ src/strings/TestRegularExpression.java
@@ -0,0 +1,27 @@
+package strings;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static util.Print.print;
+
+public class TestRegularExpression {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            print("Usage:\njava TestRegularExpression " +
+                    "characterSequence regularExpression+");
+            System.exit(0);
+        }
+        print("Input: \"" + args[0] + "\"");
+        for (String arg : args) {
+            print("Regular expression: \"" + arg + "\n");
+            Pattern p = Pattern.compile(arg);
+            Matcher m = p.matcher(args[0]);
+            while (m.find()) {
+                print("Match \"" + m.group() + "\" at positions" +
+                        m.start() + "-" + (m.end() - 1));
+            }
+
+        }
+    }
+}
---
+++ src/util/Hex.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.io.File;
+
+public class Hex {
+    public static String format(byte[] data) {
+        StringBuilder result = new StringBuilder();
+        int n = 0;
+        for (byte b : data) {
+            if (b % 16 == 0)
+                result.append(String.format("%05X: ", n));
+            result.append(String.format("%02X: ", b));
+            n++;
+            if (n % 16 == 0) result.append("\n");
+        }
+        result.append("\n");
+        return result.toString();
+    }
+
+    public static void main(String[] args) throws Exception{
+        if (args.length == 0)
+            System.out.println(
+                    format(BinaryFile.read("Hex.class"))
+            );
+        else
+            System.out.println(
+                    format(BinaryFile.read(new File(args[0])))
+            );
+    }
+}
